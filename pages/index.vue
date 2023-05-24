ea71afb171f72215aba8bb383705038050d897db
yxb94616 <13121189733@163.com>
2023-05-24 16:00:56 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex h-screen w-screen flex-col items-center justify-center">
+    <h1 class="text-6xl font-semibold text-sky-400">2023 ikun</h1>
+    <p class="mt-4 text-9xl font-bold text-gray-600">只因你太美</p>
+  </div>
+</template>

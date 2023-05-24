9d64dadeeec74247814ec7a230ae97da2329c6fe
yxb94616 <13121189733@163.com>
2023-05-24 16:04:36 +0800

    feat: Add ThemeToggle component
    - add ThemeToggle component
    - update tailwind.config.js dark mode config

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="flex h-screen w-screen flex-col items-center justify-center">
+  <div
+    class="dark:bg-dark flex h-screen w-screen flex-col items-center justify-center bg-white"
+  >
     <h1 class="text-6xl font-semibold text-sky-400">2023 ikun</h1>
     <p class="mt-4 text-9xl font-bold text-gray-600">只因你太美</p>
+    <ThemeToggle />
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
@@ -9,7 +10,11 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "rgb(18,18,18)",
+      },
+    },
   },
   plugins: [],
 };
